647eb36770d32051cc4733b2e7f6d3b53b076fbf
Rumba19 <brumba30721@ucumberlands.edu>
2025-02-08 23:06:38 -0800

    Stack pop/push

--- selection_algorithm.py
+++ selection_algorithm.py
@@ -23,9 +23,47 @@ def randomized_select(arr, low, high, k):
     else:
         return randomized_select(arr, p + 1, high, k)
 
+def median_of_medians(arr, k):
+    if len(arr) <= 5:
+        return sorted(arr)[k]
+    sublists = [arr[i:i + 5] for i in range(0, len(arr), 5)]
+    medians = [sorted(sublist)[len(sublist) // 2] for sublist in sublists]
+    median_of_median = median_of_medians(medians, len(medians) // 2)
+    less = [x for x in arr if x < median_of_median]
+    equal = [x for x in arr if x == median_of_median]
+    greater = [x for x in arr if x > median_of_median]
+    if k < len(less):
+        return median_of_medians(less, k)
+    elif k < len(less) + len(equal):
+        return median_of_median
+    else:
+        return median_of_medians(greater, k - len(less) - len(equal))
+    
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if not self.is_empty():
+            return self.stack.pop()
+    def peek(self):
+        return self.stack[-1] if not self.is_empty() else None
+    def is_empty(self):
+        return len(self.stack) == 0
+
 
 if __name__ == "__main__":
     arr = [31, 7, 19, 3, 8, 4, 8, 9, 3]
     k = 3
     print(f"{k}th smallest element (Randomized Select):", randomized_select(arr[:], 0, len(arr)-1, k-1))
- 
+    print(f"{k}th smallest element (Median of Medians):", median_of_medians(arr[:], k-1))
+
+
+#Output for stack
+    stack = Stack()
+    stack.push(33)
+    stack.push(30)
+    print("Stack Pop:", stack.pop())
